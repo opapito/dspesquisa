18ac0b497ec05eaab39198ec81e99ef8193636bb
opapito <51033778+opapito@users.noreply.github.com>
2020-09-18 13:00:14 -0700

    DTO, Service

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/GameResource.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dspesquisa.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dspesquisa.dto.GameDTO;
+import com.devsuperior.dspesquisa.services.GameService;
+
+//API REST - WEB SERVER
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameResource {
+	
+	//@Autowired //automated dependency injection
+	//private GameRepository gameRepository;
+	
+	@Autowired //automated dependency injection (instantiation)
+	private GameService service;
+	
+	// REST endpoint for games
+	@GetMapping
+	public ResponseEntity<List<GameDTO>> findAll(){
+		List<GameDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
