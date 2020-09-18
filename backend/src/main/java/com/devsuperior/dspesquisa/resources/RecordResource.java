9a0ca7939b8b7d0565ee9aafb6f20b60ed6c7841
opapito <51033778+opapito@users.noreply.github.com>
2020-09-18 13:37:14 -0700

    POST records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dspesquisa.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.services.RecordService;
+
+//API REST - WEB SERVER
+
+@RestController
+@RequestMapping(value = "/records")
+public class RecordResource {
+	
+	//@Autowired //automated dependency injection
+	//private GameRepository gameRepository;
+	
+	@Autowired //automated dependency injection (instantiation)
+	private RecordService service;
+	
+	// REST endpoint for games
+	@PostMapping
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
+		RecordDTO newDTO = service.insert(dto);
+		return ResponseEntity.ok().body(newDTO);
+	}
+}
