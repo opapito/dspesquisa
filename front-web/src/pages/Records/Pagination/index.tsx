0ad69aaa15adc9838f448b4c9908702956607ce7
opapito <51033778+opapito@users.noreply.github.com>
2020-09-20 14:25:51 -0700

    record list page created

---
+++ front-web/src/pages/Records/Pagination/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './styles.css';
+
+// the "?" prevents the app crash as it returns undefined while data is still being loaded 
+type Props = {
+  totalPages?: number;
+  goToPage: Function;
+  activePage: number;
+}
+
+const Pagination = ({ totalPages = 0, goToPage, activePage }: Props) => {
+  const paginationItems = Array.from(Array(totalPages).keys());
+
+  return (
+    <div className="pagination-container">
+     {paginationItems.map(item =>(
+        <button
+          key={item}
+          className={`pagination-item ${activePage === item ? 'active' : 'inactive'}`}
+          onClick={() => goToPage(item)}
+        >
+        {item + 1}
+       </button>
+     ))}
+    </div>
+
+  );
+}
+
+export default Pagination;
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import { RecordsResponse } from './types';
+import axios from 'axios';
+import { formatDate } from './helpers';
+import Pagination from './Pagination';
+import { Link } from 'react-router-dom'
 
-const Records = () => (
-  <h1> Records page </h1>
-);
+const BASE_URL = 'http://localhost:8080';
+
+const Records = () => {
+
+  // Internal state, useState -> React Hooks 
+  const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+  const [activePage, setActivePage] = useState(0);
+
+  // React Hooks
+  useEffect(() => {
+
+    axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+      .then(response => setRecordsResponse(response.data));
+
+  }, [activePage]); // every time activePage changes, the axios will make a request
+
+  const handlePageChange = (index: number) => {
+    setActivePage(index)
+  }
+
+
+  return (
+    <div className="page-container">
+      <div className="filters-container records-actions">
+        <Link to="/charts">
+          <button className="action-filters">
+            SEE GRAPHS
+          </button>
+        </Link>
+      </div>
+      <table className="records-table" cellPadding="0" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>INSTANT</th>
+            <th>NAME</th>
+            <th>AGE</th>
+            <th>PLATFORM</th>
+            <th>GENRE</th>
+            <th>TITLE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recordsResponse?.content.map(record => (
+            <tr key={record.id}>
+              <td>
+                {formatDate(record.moment)}
+            </td>
+              <td>
+                {record.name}
+            </td>
+              <td>
+                {record.age}
+            </td>
+              <td className="text-secondary">
+                {record.gamePlatform}
+            </td>
+              <td>
+                {record.genreName}
+            </td>
+              <td className="text-primary">
+                {record.gameTitle}
+            </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+            <Pagination
+              activePage={activePage}
+              goToPage={handlePageChange}
+              totalPages={recordsResponse?.totalPages}
+
+            />
+    </div>
+  );
+}
 
 export default Records;
