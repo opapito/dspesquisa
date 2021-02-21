2c702a9de9d6ca17d1f90aedff91cbb19b69a18e
opapito <ubuntu@windows.com>
2021-02-21 13:45:22 -0800

    add Google Fonts

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  AppLoading  from 'expo-app-loading';
+import {
+  useFonts,
+  Play_400Regular,
+  Play_700Bold
+} from '@expo-google-fonts/play';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded] = useFonts({
+      Play_400Regular,
+      Play_700Bold    
+    });
+
+    if (!fontsLoaded){
+      return <AppLoading />
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.text}>Hello world!</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: '#FFF',
+    fontSize: 50
+  }
 });
