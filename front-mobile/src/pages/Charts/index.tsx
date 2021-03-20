fdde5e6a302ce6745bde8cd7e79cd15915f69a87
opapito <51033778+opapito@users.noreply.github.com>
2021-03-19 20:12:06 -0700

    feat(Charts): add charts

--- front-mobile/src/pages/Charts/index.tsx
+++ front-mobile/src/pages/Charts/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, SafeAreaView, ScrollView } from 'react-native';
 import Header from '../../components/Header';
 import axios from 'axios';
 import {buildBarSeries, getPlatformChartData, getGenderChartData} from './helpers';
-import { VictoryBar, VictoryAxis, VictoryChart, VictoryPie, VictoryTheme } from 'victory-native';
+import { VictoryBar, VictoryChart, VictoryPie, VictoryGroup, VictoryLabel} from 'victory-native';
 import { PieChartData, BarChartData } from '../../pages/Types'
+
 
 
 
@@ -16,6 +17,14 @@ const initialPieData = {
 const BASE_URL = 'http://192.168.1.13:8080';
 //! It is not possible to use localhost with expo. It is necessary to use the same ip of the browser expo running application
 
+const CenteredLabel = (props:any) => {
+  const { datum, scale } = props;
+  const centerPos = scale.y(datum._y / 2);
+  const style = { fill: "white", textAnchor: "end" };
+  // note that because your chart is horizontal,
+  // the y value that you calculate informsthe x position of the label
+  return <VictoryLabel {...props} x={centerPos} style={style} dx={0} />;
+};
 
 const Charts = () => {
     // creating three states for each graph
@@ -57,39 +66,59 @@ const Charts = () => {
   return (
     <>
       <Header />
-        <View style={styles.container}>
-          <VictoryChart
-            // adding the material theme provided with Victory
-            theme={VictoryTheme.material}
-            domainPadding={20}
-          >
-            <VictoryAxis
-              tickValues={[1, 2, 3, 4]}
-              tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-            />
-            <VictoryAxis
-              dependentAxis
-              tickFormat={(x) => (`$${x / 1000}k`)}
-            />
-            <VictoryBar
-              data={data}
-              x="quarter"
-              y="earnings"
-            />
-          </VictoryChart>
-          <VictoryPie
-              colorScale={["#a8dadc", "#ed7947", "#00D4FF", "#ffd6a5"]}
+       <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.insideView}>
+            <VictoryGroup
+              // adding the material theme provided with Victory
               //theme={VictoryTheme.material}
-              innerRadius={68} labelRadius={80}
-            data={platformData}
-          />
-          <VictoryPie
-              colorScale={["#a8dadc", "#ed7947", "#00D4FF", "#ffd6a5"]}
-              //theme={VictoryTheme.material}
-              innerRadius={68} labelRadius={80}
-            data={genderData}
-          />
-        </View>
+              domainPadding={20}
+            >
+
+              <VictoryBar horizontal
+               data={data}
+               x="quarter"
+               y="earnings"
+               labels={({ datum })=> datum._x}
+               style={{                 
+                 data: { fill: "#ed7947" }
+                }}
+            
+                labelComponent={<CenteredLabel />}
+                
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 }
+                }}
+
+
+              />
+            </VictoryGroup>
+            <VictoryPie
+                colorScale={["#a8dadc", "#ed7947", "#00D4FF", "#ffd6a5"]}
+                //theme={VictoryTheme.material}
+                innerRadius={68} labelRadius={80}
+              data={platformData}
+              animate={{
+                duration: 2000,
+                onLoad: { duration: 1000 }
+              }}
+
+            />
+            <VictoryPie
+                colorScale={["#a8dadc", "#ed7947", "#00D4FF", "#ffd6a5"]}
+                //theme={VictoryTheme.material}
+                innerRadius={68} labelRadius={80}
+              data={genderData}
+              animate={{
+                duration: 2000,
+                onLoad: { duration: 1000 }
+              }}
+
+            />
+          </View>
+         </ScrollView>
+        </SafeAreaView>
     </>
   );
 
@@ -100,6 +129,14 @@ export default Charts;
 const styles = StyleSheet.create({
   container: {
     flex:1,
+  },
+  scrollView: {
+    //backgroundColor: 'pink',
+    marginHorizontal:0
+    
+  },
+  insideView:{
+    marginHorizontal: 20,
     paddingRight: '5%',
     paddingLeft: '5%',
     paddingBottom: 50,
