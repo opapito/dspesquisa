a980646f8fea25d8d0dc84534d847d9e60716823
opapito <51033778+opapito@users.noreply.github.com>
2021-03-23 09:47:57 -0700

    chore(README): typos and pictures fix

--- front-web/src/pages/Records/helpers.ts
+++ front-web/src/pages/Records/helpers.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export const formatDate =  (date: string) =>{
-  return moment.parseZone(date).format('DD/MM/YYYY HH:mm');
+  return moment.parseZone(date).format('MM/DD/YYYY HH:mm');
   /*
    !If your date format has a fixed timezone offset, use moment.parseZone:
    * see https://momentjs.com/guides/#/parsing/local-utc-zone/
